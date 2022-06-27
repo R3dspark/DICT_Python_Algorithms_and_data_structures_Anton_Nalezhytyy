edf35b334535d211c163400be527e9038a89d632
R3dspark <a.o.nalezhytyy@student.khai.edu>
2022-06-27 23:51:42 +0300

    Update ram_specification.py

--- Praktik2/ram_specification.py
+++ Praktik2/ram_specification.py
@@ -1,9 +1,8 @@
 corsair = {"ValueSelect": 2008,
-           "VengeanceLP": 2012,
-           "VengeancePro": [2012, 2016],
-           ("VengeanceLPX", "DominatorPlatinum"): 2016}
-hyperX = {"Fury": [2012, 2016],
+           "VengeanceLP": 2012, "VengeancePro": 2012,
+           "VengeancePRO": 2016, "VengeanceLPX": 2016, "Dominator": 2016}
+hyperX = {"Fury": 2012, "FURY": 2016,
           "Predator": 2016}
-corsair_type = {"ValueSelect": ["DDR2"], "VengeanceLP": ["DDR3"], "VengeancePro": ["DDR3", "DDR4"],
-("VengeanceLPX", "DominatorPlatinum"): ["DDR4"]}
-hyperX_type = {"Fury": ["DDR3", "DDR4"], "Predator": ["DDR4"]}
+corsair_type = {"ValueSelect": ["DDR2"], "VengeanceLP": ["DDR3"], "VengeancePro": ["DDR3"], "VengeancePRO": ["DDR4"],
+                "VengeanceLPX": ["DDR4"], "Dominator": ["DDR4"]}
+hyperX_type = {"Fury": ["DDR3"], "FURY": ["DDR4"], "Predator": ["DDR4"]}
